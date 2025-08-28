0c1d335e0bbded5b89288402ffeaa6414d59046b
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-08-28 15:28:39 +0400

    fix[core]: add null placeholder in case of no details

--- libs/core/src/utils/sendError.ts
+++ libs/core/src/utils/sendError.ts
@@ -3,7 +3,9 @@ import { FastifyReply } from 'fastify';
 const sendError = (reply: FastifyReply, code: number, errorCode: string, message: string, details?: object) => {
 	return reply.code(code).send({
 		status: 'error',
-		error: { code: errorCode, message, details }
+		error: {
+			code: errorCode, message, details: details ?? null
+		}
 	});
 }
 
